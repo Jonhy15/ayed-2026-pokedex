a8d3f8e943183f23d1a079abbcc2def3b8df2334
jesicab-arias <ariasjesicabrenda@gmail.com>
2026-09-20 22:38:16 -0300

    Update pokemon.py

--- src/dominio/pokemon.py
+++ src/dominio/pokemon.py
@@ -110,3 +110,10 @@ catalogo = [
         "generacion": 1
     }
 ]
+class Pokemon:
+def __init__(self, nombre, hp, ataque):
+self.nombre = nombre
+self.hp = hp
+self.ataque = ataque
+def resumen(self):
+return f"{self.nombre} (hp {self.hp})
